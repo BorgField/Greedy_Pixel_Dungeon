623caff26e095d7e3fba181e91f30a2fdbd7f430
BorgField <147969784+BorgField@users.noreply.github.com>
2025-03-23 00:06:01 +0800

    上传文件

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/CatCookie.java
@@ -0,0 +1,10 @@
+package com.shatteredpixel.shatteredpixeldungeon.items.food;
+
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Hunger;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+public class CatCookie {
+
+
+}
